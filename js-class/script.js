e23ed39f6af597d794046ff0713ff7685b1ec82f
chukwuekwu-Daniel01 <emekedaniel169@gmail.com>
2026-05-19 13:57:28 -0700

    i added my assignment folder and completed two of my assignment

---
+++ js-class/script.js
@@ -0,0 +1,37 @@
+// let name = "mercy"
+// let age = 19
+// let occupation = "student"
+// console.log(`Your name is ${name}, you are ${age} years old and you are a ${occupation}`)
+
+// let school = "delsu";
+// let address = "Asaba"
+
+// console.log(`The name of my school is ${school}, it located at ${address}`)
+
+
+// 1. the length method(used for a specific number)
+
+// let title = "javascript"
+// let formattedText = title.length
+// console.log(`${formattedText}`)
+
+//2. the .toUppercase and toLowercase method (changes text to uppercase or lowercase)
+// let title = "javascript"
+// let formattedText = title.toLowerCase()
+// console.log(`${formattedText}`)
+
+
+//4. slice method (the first index and how many you want to take)
+// let title = "javascript"
+// let formattedText = title.slice(3)
+// console.log(`${formattedText}`)
+
+
+//5. .includes (this talk about what was included and order, example should be seen and practiced below)
+let title = "javascript"
+let formattedText = title.includes('ast')
+console.log(`${formattedText}`)
+
+
+
+
