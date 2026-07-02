64837cceccaeda6354f332ed6db59d1bf0323d0d
Treasure Audu <treasureaudu7@gmail.com>
2026-07-02 14:40:34 +0100

    added sql operation to the oop2 file

--- oop2/Product.php
+++ oop2/Product.php
@@ -3,12 +3,13 @@ include("BasicOperation.php");
 include("Database.php");
 class Product extends Database{
     protected string $table = "products";
+    protected array $fillable = ["title", "price", "description"];
      function __construct()
     {
         parent::__construct();
     }
-   use BasicOperation;
+   use BasicOperation, Notification;
 }
 
 $coke = new Product();
-$coke->add("products");
+$coke->add("spirte", "400", "very sweet",);
